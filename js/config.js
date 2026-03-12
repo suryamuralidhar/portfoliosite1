21f1ebf1ddf877d1d240cc8fb99e4f142603e6be
suryamuralidhar <suryamurali006@gmail.com>
2026-03-12 17:19:53 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,29 @@
+//Rev2
+
+
+const CONFIG = {
+
+title: "Villa Interior – Living Room",
+
+heading: "Living Room – Villa Interior",
+
+panoramaImage: "1.png",
+
+popupText: `
+<b>Disclaimer</b><br><br>
+You are viewing this on a mobile device,
+please rotate your phone to <b>Landscape</b>
+and use <b>Fullscreen</b> for the best experience.
+`,
+
+popupButton: "OK 😃",
+
+mobileWidth: 900,
+
+popupDelay: 1000,
+
+dragFriction: 0.5
+
+};
+
+
